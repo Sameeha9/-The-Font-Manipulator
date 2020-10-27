a17ce0b8b8f7ad75835b6985f8d828d3ddfd0ed3
Sameeha9 <71824940+Sameeha9@users.noreply.github.com>
2020-10-27 19:47:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-noseX=0;
-noseY=0;
 difference = 0;
 rightWristX = 0;
 leftWristX = 0;
@@ -25,9 +23,6 @@ function gotPoses(results)
   if(results.length > 0)
   {
     console.log(results);
-    noseX = results[0].pose.nose.x;
-    noseY = results[0].pose.nose.y;
-    console.log("noseX = " + noseX +" noseY = " + noseY);
 
     leftWristX = results[0].pose.leftWrist.x;
     rightWristX = results[0].pose.rightWrist.x;
@@ -38,10 +33,10 @@ function gotPoses(results)
 }
 
 function draw() {
-background('#969A97');
+background('#FFB6C1');
 
-  document.getElementById("square_side").innerHTML = "Width And Height of a Square will be = " + difference +"px";
-  fill('#F90093');
-  stroke('#F90093');
-  square(noseX, noseY, difference);
+  document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference +"px";
+textSize(difference);
+fill('#FF0000');
+text('Welcome', 50, 400);
 }
